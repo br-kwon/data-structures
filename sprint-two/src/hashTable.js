847d9e796ebf163dd29894b89a415c8ac8176252
br-kwon <brian.y.kwon@gmail.com>
2015-10-08 16:34:14 -0700

    Write bare minimum functions for hashTable and all specs passing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,17 +6,41 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  console.log('insert called');
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var value = this._storage.get(index);
+
+  if(!value){
+    value = {};
+  }
+  value[k] = v;
+  console.log(value);
+  this._storage.set(index, value);
 };
 
 HashTable.prototype.retrieve = function(k) {
+
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  if (!this._storage.get(index)) {
+    return null;
+  }
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(value, i, array){
+    if(i === index){
+      array[i] = undefined;
+    }
+  });
 };
 
+var storages = {
+
+}
 
 
 /*
