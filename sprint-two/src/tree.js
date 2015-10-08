69d39833a1ca966b3e54b2db19938fa3b07acb4c
br-kwon <brian.y.kwon@gmail.com>
2015-10-08 10:31:45 -0700

    Wrote tree with functional shared and all tests passing

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -5,6 +5,8 @@ var Tree = function(value) {
   // your code here
   newTree.children = null;  // fix me
 
+  _.extend(newTree, treeMethods);
+
   return newTree;
 };
 
@@ -12,10 +14,36 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   // your code here
+  var newTree = Tree(value);
   newTree.children = null;  // fix me
+
+  if(this.children === null){
+    this.children = [newTree];
+  }else{
+    this.children.push(newTree);
+  }
+  
 };
 
 treeMethods.contains = function(target) {
+
+  var wasFound = false;
+
+  var check = function(node) {
+    if (node.value === target) {
+      wasFound = true;
+    }
+    if (node.children !== null) {
+      _.each(node.children, function(child) {
+        check(child);
+      })
+    }
+  };
+
+  check(this);
+
+  return wasFound;
+
 };
 
 
