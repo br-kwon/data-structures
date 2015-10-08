7c9d15f9ea289746d85984e8e190cdcd44ff1d3e
br-kwon <brian.y.kwon@gmail.com>
2015-10-08 10:10:29 -0700

    Worked out linkedlist and passed all specs

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,43 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+
+    if(list.head === null){
+      list.head = node;
+    }else{
+      list.tail.next = node;
+    }
+
+    list.tail = node;
+    
   };
 
   list.removeHead = function() {
+
+    var returnValue = list.head.value;
+    var tempNode = list.head.next;
+    list.head.next = null;
+    list.head = tempNode;
+    return returnValue;
+
   };
 
   list.contains = function(target) {
+    var wasFound = false;
+
+    var check = function(node){
+      if(node === null){
+        return;
+      }
+      if(node.value === target){
+        wasFound = true;
+      }
+      check(node.next);
+    }
+    check(list.head);
+
+    return wasFound;
   };
 
   return list;
