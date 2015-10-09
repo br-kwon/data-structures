a309ec583488e60edccfc81323cd69ca5d689fe4
br-kwon <brian.y.kwon@gmail.com>
2015-10-09 12:38:19 -0700

    Implemented HashTable doubling in size

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,23 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.numTuples = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(index);
-
-  if(!bucket){
-    bucket = [];
-  }
+  var bucket = this._storage.get(index) || [];
 
   bucket.push([k, v]);
   this._storage.set(index, bucket);
+  
+  this.numTuples++;
+  
+  if(this.needsDoubling()){
+    this._limit *= 2;
+    this.reHashStorage();
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -42,6 +47,22 @@ HashTable.prototype.remove = function(k) {
     }
   });
 };
+
+HashTable.prototype.needsDoubling = function(){
+  return this.numTuples / this._limit > 0.75;
+}
+
+HashTable.prototype.reHashStorage = function(){
+  var oldStorage = this._storage;
+  this._storage = LimitedArray(this._limit);
+  this.numTuples = 0;
+  var self = this;
+  oldStorage.each(function(bucket, i, storage){
+    _.each(bucket, function(tuple) {
+      self.insert(tuple[0], tuple[1]);
+    })
+  })
+}
 
 
 /*
